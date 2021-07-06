faf02560db228936138f7fa1a881eb76dfda76a5
Florian LEROUX <florian.leroux3@laposte.net>
2021-07-06 11:10:05 +0200

    feat(create): add tweet creation

--- pages/create.js
+++ pages/create.js
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+import firebase from "../lib/firebase.config";
+
 export default function CreateTweetPage() {
-  return "cc";
+  const [content, setContent] = useState("");
+
+  const sendTweet = async () => {
+    const tweet = await firebase.firestore().collection("tweets").add({
+      content,
+      creator: "test",
+      date: new Date(),
+    });
+  };
+
+  return (
+    <div className="flex flex-col p-5 space-y-3">
+      <label>
+        Contenu du tweet
+        <Input
+          placeholder="Contenu"
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+        />
+      </label>
+      <Button onClick={sendTweet}>Envoyer</Button>
+    </div>
+  );
 }
